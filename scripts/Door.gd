7f610ce01459393b6adaf01f2c2fe381e6407a7d
radix-r <20172491+rosscwagner@users.noreply.github.com>
2023-02-25 14:12:32 -0500

    Double doors open in the same direction

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -2,22 +2,24 @@ extends StaticBody
 
 onready var tween = $Tween
 
+# directionthe door opens. 
+export var direction: int = 1
+
 var open: bool = false
 var in_animation: bool = false
 var open_speed: float = 0.5
 
 func interact():
-    
-    if !open and !in_animation:
-        tween.interpolate_property(self, "rotation_degrees", Vector3(0, rotation_degrees.y, 0), Vector3(0, rotation_degrees.y+90, 0), open_speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-        
-    elif open and !in_animation:
-        tween.interpolate_property(self, "rotation_degrees", Vector3(0, rotation_degrees.y, 0), Vector3(0, rotation_degrees.y-90, 0), open_speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-    
-    in_animation = true
-    tween.start()
+		
+	
+	if !in_animation:
+		tween.interpolate_property(self, "rotation_degrees", Vector3(rotation_degrees.x, rotation_degrees.y, rotation_degrees.z), Vector3(rotation_degrees.x, rotation_degrees.y+90*direction, rotation_degrees.z), open_speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		direction = direction * -1
+	
+	in_animation = true
+	tween.start()
 
 func _on_Tween_tween_all_completed() -> void:
-    open = !open
-    in_animation = false
+	open = !open
+	in_animation = false
 
