31d48f5fd18e8616bde7e4cadb3ddb340982f1bb
radix-r <20172491+radix-r@users.noreply.github.com>
2023-04-18 15:32:23 -0400

    Door scaling progress

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -5,9 +5,18 @@ extends Node3D
 
 var door_force: float = 0.8
 
+func _ready() -> void:
+	if scale != Vector3.ONE:
+		$PhysicsDoor/CollisionShape3D.scale = scale
+		
+		$PhysicsDoor/MeshInstance3D.scale = scale
+		
+		
+
 func _physics_process(_delta: float) -> void:
-	var door_angle: float = door.global_rotation_degrees.y
-	
+	var door_angle: float = door.rotation_degrees.y
+	door.rotation_degrees.x = 0
+	door.rotation_degrees.z = 0
 	if abs(door_angle) > 5:
 		hinge.set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY, -1 * door_angle/abs(door_angle) * door_force )
 	else:
