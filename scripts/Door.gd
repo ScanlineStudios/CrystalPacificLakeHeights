acd6759a7cf1c7c865a46f7172e2a9300eba17b1
radix-r <20172491+radix-r@users.noreply.github.com>
2023-04-24 16:25:05 -0400

    Scaled doors working

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -3,14 +3,21 @@ extends Node3D
 @onready var hinge: HingeJoint3D = $HingeJoint3D
 @onready var door: PhysicsBody3D = $PhysicsDoor
 
+
 var door_force: float = 0.8
 
 func _ready() -> void:
 	if scale != Vector3.ONE:
+		
+		var mesh_size: Vector3 = $PhysicsDoor/MeshInstance3D.mesh.get_size()
+
 		$PhysicsDoor/CollisionShape3D.scale = scale
+		$PhysicsDoor/CollisionShape3D. transform.origin.x += ((scale.x - 1) * mesh_size.x) / 2
 		
 		$PhysicsDoor/MeshInstance3D.scale = scale
+		$PhysicsDoor/MeshInstance3D.transform.origin.x += ((scale.x - 1) * mesh_size.x) / 2
 		
+		door.transform.origin.x +=  ((scale.x - 1) * mesh_size.x) / 2
 		
 
 func _physics_process(_delta: float) -> void:
